c25da3cc79be89608efd699bbdb3250f5ec5374e
yj-shrest <shresthayujal46@gmail.com>
2023-09-03 21:54:36 +0545

    push

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def handle_message(data):
     emit('message', message, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, debug=False,host='0.0.0.0')
